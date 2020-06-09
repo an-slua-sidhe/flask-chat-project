34a1c81845d662e475292d23a70b13159ca66183
an-slua-sidhe <polocoinn@yahoo.ie>
2020-06-09 16:54:31 +0100

    Add index.html and time stamp functionality to run.py

--- run.py
+++ run.py
@@ -1,33 +1,43 @@
 import os
-from flask import Flask, redirect
+from datetime import datetime
+from flask import Flask, redirect, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = "randomstring123"
 messages = []
 
 def add_messages(username, message):
-    """Add messages to the 'messages' list"""
-    messages.append("{}: {}".format(username, message))
+    """Add messages to the `messages` list"""
+    now = datetime.now().strftime("%H:%M:%S")
+    messages.append("({}) {}: {}".format(now, username, message))
 
 def get_all_messages():
-    """Get all of the messages and separate them with a 'br'"""
+    """Get all of the messages and separate them with a `br`"""
     return "<br>".join(messages)
 
-
-@app.route('/')
+@app.route("/", methods=["GET", "POST"])
 def index():
     """Main page with instructions"""
-    return "To send a message use: /USERNAME/MESSAGE"
+    if request.method == "POST":
+        session["username"] = request.form["username"]
 
-@app.route('/<username>')
+    if "username" in session:
+        return redirect(session["username"])
+
+    return render_template("index.html")
+
+
+@app.route("/<username>")
 def user(username):
     """Display chat messages"""
-    return "<h1>Welcome, {0}<h1>{1}".format(username, get_all_messages())
+    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
 
-@app.route('/<username>/<message>')
+
+@app.route("/<username>/<message>")
 def send_message(username, message):
     """Create a new message and redirect back to the chat page"""
     add_messages(username, message)
     return redirect("/" + username)
 
 
-app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True)
+app.run(host=os.getenv("IP"), port=os.getenv("PORT"), debug=True)
